606a6a911b459b8595d7cc270ae7f6f0b5aaaf07
phongdtgch190859 <dothanhphongcb11@gmail.com>
2022-04-12 03:03:35 +0700

    login security

--- databaseHandler.js
+++ databaseHandler.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs/dist/bcrypt");
+const async = require("hbs/lib/async");
 const { MongoClient, ObjectId } = require("mongodb");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
@@ -6,52 +8,54 @@ const URL =
   "mongodb+srv://thanhbinh2001:123456789abc@cluster0.spshl.mongodb.net/test";
 const DATABASE_NAME = "Shop_Demo";
 
+
 async function getdbo() {
-  const client = await MongoClient.connect(URL);
-  const dbo = client.db(DATABASE_NAME);
-  return dbo;
+    const client = await MongoClient.connect(URL);
+    const dbo = client.db(DATABASE_NAME);
+    return dbo;
 }
+
 async function insertObject(collectionName, objectToInsert) {
     const dbo = await getdbo();
     const newObject = await dbo
-      .collection(collectionName)
-      .insertOne(objectToInsert);
+    .collection(collectionName)
+    .insertOne(objectToInsert);
     console.log(
-      "Gia tri id moi duoc insert la: ",
-      newObject.insertedId.toHexString()
+    "Gia tri id moi duoc insert la: ",
+    newObject.insertedId.toHexString()
     );
 }
-
+  
 async function checkUserLogin(nameIn) {
     const dbo = await getdbo();
     const results = await dbo.collection("Users").findOne({ userName: nameIn });
     if (results) {
-      return results;
+    return results;
     } else {
-      return -1;
+    return -1;
     }
 }
-
+  
 async function checkUserRole(nameIn) {
     const dbo = await getdbo();
     const user = await dbo.collection("Users").findOne({ userName: nameIn });
     if (user == null) {
-      return -1;
+    return -1;
     } else {
-      return user.role;
+    return user.role;
     }
 }
-
+  
 async function checkUser(nameIn) {
     const dbo = await getdbo();
     const results = await dbo.collection("Users").findOne({ userName: nameIn });
     if (results != null) {
-      return true;
+    return true;
     } else {
-      return false;
+    return false;
     }
 }
-
+  
 async function getUser(name) {
     const dbo = await getdbo();
     const result = await dbo.collection("Users").findOne({ userName: name });
@@ -64,4 +68,4 @@ module.exports = {
     checkUser,
     checkUserLogin,
     getUser,
-  };
+};
--- require_login.js
+++
@@ -1,8 +0,0 @@
-function requiresLogin(req,res,next){
-    if(req.session["User"]){
-        return next()
-    }else{
-        res.redirect('/login')
-    }
-}
-module.exports = { requiresLogin}
