5f28e5bfb16307d3bb591a1a1575413a4af66b79
binbin2001 <89694531+binbin2001@users.noreply.github.com>
2022-04-25 13:52:57 +0700

    reply

--- Controllers/admin.js
+++ Controllers/admin.js
@@ -20,7 +20,7 @@ router.use((req, res, next) => {
 
 
 //neu request la: /admin
-router.get('/', async (req, res) => {
+router.get('/', async(req, res) => {
     if (req.query.sortBy == "today") { //if user choose today
         res.redirect("/admin/today");
     } else if (req.query.sortBy == 'week') { //if user choose week
@@ -34,20 +34,24 @@ router.get('/', async (req, res) => {
                 element.itemString += e.name + " - (" + e.qty + ")"; //display name + qty 
             })
         });
+        const statisticalResult = await dbHandler.countStatistical();
         res.render('adminPage', {
-            customerOrder: customerOrder,//truyen vao adminPage giá trị của customerorder
-            user: req.session.user//
-        })
-    // }
-}})
+                customerOrder: customerOrder, //truyen vao adminPage giá trị của customerorder
+                user: req.session.user, //
+                statistical: statisticalResult
+            })
+            // }
+    }
+})
 
-router.get("/:sortBy", async (req, res, next) => { //sortby same tham số
+router.get("/:sortBy", async(req, res, next) => { //sortby same tham số
     let result = await dbHandler.getAll("Customer Order");
+    const statisticalResult = await dbHandler.countStatistical();
     if (req.params.sortBy === "today") {
-        let today = new Date().toLocaleDateString("vi");//lưu new Date.toLocaledatestring = today; today ở dạng stirng
+        let today = new Date().toLocaleDateString("vi"); //lưu new Date.toLocaledatestring = today; today ở dạng stirng
         result = result.filter((item) => { //dùng filter cho biến result lọc ra các phần từ có đk là dòng 53. sau đó gán lại vào result
             item.itemString = "";
-            item.books.forEach(e => {//tao bien itemString de hien thi cac phan tu trong element (them item va amount)
+            item.books.forEach(e => { //tao bien itemString de hien thi cac phan tu trong element (them item va amount)
                 item.itemString += e.name + " - (" + e.qty + ")";
             })
             return item.time.toLocaleDateString("vi") === today
@@ -55,82 +59,95 @@ router.get("/:sortBy", async (req, res, next) => { //sortby same tham số
         result.forEach((element) => { //dùng loop cho tất cả result để hiển thị time theo dang string
             element.time = element.time.toLocaleString("vi");
         });
-        res.render("adminPage", { customerOrder: result }); //truyền vào tất cả các result vừa được xử lý
+        res.render("adminPage", { customerOrder: result, statistical: statisticalResult }); //truyền vào tất cả các result vừa được xử lý
 
     } else if (req.params.sortBy === "week") {
         let today = new Date(); //gán today bằng ngày hn
         let week = new Date(today.setDate(today.getDate() - 7)); //week = lấy (today.getDate -7) là dạng số. sau đó setDate lại về dạng thời gian
         result = result.filter((item) => { //item là 1 phần tử của result
             item.itemString = "";
-            item.books.forEach(e => {//tao bien itemString de hien thi cac phan tu trong element (them item va amount)
+            item.books.forEach(e => { //tao bien itemString de hien thi cac phan tu trong element (them item va amount)
                 item.itemString += e.name + " - (" + e.qty + ")";
             })
             return item.time > week //dk ptu có điều kiện item > week
         });
-        result.forEach((element) => {//dùng loop cho result sau đó chuyển element.time về dạng string theo keieur vn
+        result.forEach((element) => { //dùng loop cho result sau đó chuyển element.time về dạng string theo keieur vn
             element.time = element.time.toLocaleString("vi");
         });
-        res.render("adminPage", { customerOrder: result }); //truyền vào tất cả result vừa được xử lý
-    }
-    else if (req.params.sortBy === "delete") {//tham số sortby là delete
+        res.render("adminPage", { customerOrder: result, statistical: statisticalResult }); //truyền vào tất cả result vừa được xử lý
+    } else if (req.params.sortBy === "delete") { //tham số sortby là delete
         let { id } = req.query; // same as: let id = req.query.id;
-        let result = await dbHandler.deleteOne("Customer Order", { _id: ObjectId(id) });//dùng hàm deOne để xóa 1 document trong collection customer order
-        if (result == null) { 
-           res.send("Cancel error!"); 
+        let result = await dbHandler.deleteOne("Customer Order", { _id: ObjectId(id) }); //dùng hàm deOne để xóa 1 document trong collection customer order
+        if (result == null) {
+            res.send("Cancel error!");
         } else {
             res.redirect("/admin");
         }
-    }
-    else {
+    } else {
         next("route");
     }
 });
 
-router.get('/manageCustomer', async (req, res) => {
+router.get('/manageCustomer', async(req, res) => {
     result = await dbHandler.getAll("Users");
+    const statisticalResult = await dbHandler.countStatistical();
     const arr = result.filter((element) => {
         return element.role === 'Customer'
     });
     arr.forEach((element, index) => {
-        element.index = index+1;
+        element.index = index + 1;
         delete element.password;
         delete element.role;
     })
-    res.render('adminPage', { Customer: arr})//truyền vào property Customer với values =arr
-    
+    res.render('adminPage', { Customer: arr, statistical: statisticalResult }) //truyền vào property Customer với values =arr
+
 })
 
 // /deleteCustomer/abcxyz
 router.get('/deleteCustomer/:id', async(req, res) => {
-    await dbHandler.deleteDocumentById('Users', req.params.id);//dugf dDBI để xóa 1 doc với tham số là req.pấm.id
+    await dbHandler.deleteDocumentById('Users', req.params.id); //dugf dDBI để xóa 1 doc với tham số là req.pấm.id
     res.redirect('/admin/manageCustomer')
-    
+
 })
 
-router.get("/feedbackManage", async (req, res) => {
+router.get("/feedbackManage", async(req, res) => {
     let result = await dbHandler.getAllFeedback();
-    res.render('adminPage', { feedback: result, user: req.session.user })//truyền vào
+    const statisticalResult = await dbHandler.countStatistical();
+    res.render('adminPage', { feedback: result, user: req.session.user, statistical: statisticalResult }) //truyền vào
 });
-
+router.post("/feedbackManage/reply", async(req, res) => {
+    console.log(req.query)
+    const feedback = await dbHandler.findFeedbackById(req.query.id);
+    console.log(feedback);
+    if (feedback == undefined || feedback == null) {
+        //error on input data
+    } else if (feedback.reply != null && feedback != undefined) {
+        //user update before
+    } else {
+        feedback.reply = req.query.reply;
+        await dbHandler.updateRepply(feedback._id, feedback);
+    }
+    res.redirect('/admin/feedbackManage');
+});
 // /feedbackManage/delete?id=abcxyz
-router.get('/feedbackManage/delete', async (req, res) => {
-    await dbHandler.deleteDocumentById('Feedback', req.query.id);//dùng req.id để nhận id của doc truyền vào hàm delete
+router.get('/feedbackManage/delete', async(req, res) => {
+    await dbHandler.deleteDocumentById('Feedback', req.query.id); //dùng req.id để nhận id của doc truyền vào hàm delete
     res.redirect('/admin/feedbackManage');
 })
 
-router.get("/feedbackManage/:day", async (req, res, next) => {
+router.get("/feedbackManage/:day", async(req, res, next) => {
     let result = await dbHandler.getAllFeedback();
     const today = new Date();
     if (req.params.day === "today") {
         result = result.filter((e) => {
-            return new Date(e.time).toDateString() === today.toDateString();//chuyển e.time về string dạng ngày 
+            return new Date(e.time).toDateString() === today.toDateString(); //chuyển e.time về string dạng ngày 
         });
         res.render("adminPage", {
             feedback: result,
             user: req.session.user,
         });
     } else if (req.params.day === "2days") {
-        const queryDay = new Date(today.setDate(today.getDate() - 2));//setdate = getdate -2 
+        const queryDay = new Date(today.setDate(today.getDate() - 2)); //setdate = getdate -2 
         result = result.filter((e) => new Date(e.time) > queryDay);
         res.render("adminPage", {
             feedback: result,
@@ -155,9 +172,9 @@ router.get("/feedbackManage/:day", async (req, res, next) => {
     }
 });
 
-router.get("/feedbackManage/specifyDay/:day", async (req, res) => {
+router.get("/feedbackManage/specifyDay/:day", async(req, res) => {
     let result = await dbHandler.getAllFeedback();
-    const specifyDay = new Date(req.params.day).toDateString();//chuyển tham số day về dáng tring
+    const specifyDay = new Date(req.params.day).toDateString(); //chuyển tham số day về dáng tring
     result = result.filter((e) => new Date(e.time).toDateString() === specifyDay);
     res.render("adminPage", { feedback: result, user: req.session.user });
 });
@@ -170,11 +187,11 @@ router.get("/feedbackManage/specifyDay/:day", async (req, res) => {
 //     res.render('adminPage', {feedback: arr})
 // })
 
-router.get("/feedbackManage/searchFeedback", async (req, res) => {
+router.get("/feedbackManage/searchFeedback", async(req, res) => {
     const searchInput = req.query.bookName;
     const result = await dbHandler.searchObjectbyName("Feedback", searchInput)
-    res.render('adminpage', {feedback: result})
-  });
+    res.render('adminpage', { feedback: result })
+});
 
 
 
@@ -183,7 +200,7 @@ router.get("/feedbackManage/searchFeedback", async (req, res) => {
 //neu request la: /admin/addUser
 router.get('/addUser', (req, res) => {
     res.send("This is add user page!")
-    // res.render('addUser')
+        // res.render('addUser')
 })
 
 //Submit add User
@@ -201,76 +218,76 @@ router.post('/addUser', (req, res) => {
 })
 
 
-router.get('/product', async (req, res) => {
+router.get('/product', async(req, res) => {
     const book = await dbHandler.getAll("Book")
+    const statisticalResult = await dbHandler.countStatistical();
+    res.render("Admin_Product", { book: book })
 
-    res.render("Admin_Product", {book:book})
-    
 });
 //addbook
-router.get('/addbook', async (req, res)=> {
+router.get('/addbook', async(req, res) => {
     res.render("AddBook")
 })
-router.post('/addbook', async (req, res) => {
-    const nameInput = req.body.txtName
-    const priceInput = req.body.txtPrice
-    const image = req.body.txtImage
-    const Description = req.body.txtDescription
-    const Category = req.body.Category
-    const CategoryID = await dbHandler.getDocumentByName("Category" , Category)
-    const newBook = {name:nameInput, des:Description, price:Number.parseFloat(priceInput), pic:image, category:CategoryID._id}
-    await dbHandler.insertObject("Book", newBook)
-    res.redirect('/admin/product')
-})
-//delete book
-router.get('/deletebook', async (req, res) => {
+router.post('/addbook', async(req, res) => {
+        const nameInput = req.body.txtName
+        const priceInput = req.body.txtPrice
+        const image = req.body.txtImage
+        const Description = req.body.txtDescription
+        const Category = req.body.Category
+        const CategoryID = await dbHandler.getDocumentByName("Category", Category)
+        const newBook = { name: nameInput, des: Description, price: Number.parseFloat(priceInput), pic: image, category: CategoryID._id }
+        await dbHandler.insertObject("Book", newBook)
+        res.redirect('/admin/product')
+    })
+    //delete book
+router.get('/deletebook', async(req, res) => {
+        const id = req.query.id
+        console.log(id)
+        await dbHandler.deleteDocumentById("Book", id)
+        res.redirect('/admin/product')
+    })
+    //update book in product
+router.get('/updatebook', async(req, res) => {
     const id = req.query.id
-    console.log(id)
-    await dbHandler.deleteDocumentById("Book", id)
-    res.redirect('/admin/product')
+    const result = await getDocumentById(id, "Book")
+    res.render('updatebook', { book: result })
 })
-//update book in product
-router.get('/updatebook', async (req, res) => {
-    const id = req.query.id
-    const result = await getDocumentById(id,"Book")
-    res.render('updatebook', {book:result})
-})
-router.post('/updatebook', async (req, res) => {
-    const nameInput = req.body.txtName
-    const priceInput = req.body.txtPrice
-    const image = req.body.txtImage
-    const Description = req.body.txtDescription
-    const Category = req.body.Category
-    const CategoryID = await dbHandler.getDocumentByName("Category" , Category)
-    const UpdateValue = {$set: {name:nameInput, des:Description, price:Number.parseFloat(priceInput), pic:image, category:CategoryID._id}}
-    const id = req.body.txtid
-    console.log(UpdateValue)
-    console.log(id)
-    await dbHandler.updateDocument(id, UpdateValue,"Book")
-    res.redirect('/admin/product')
-})
-//update profile for admin
-router.get('/updateprofile', async (req, res)=>{
+router.post('/updatebook', async(req, res) => {
+        const nameInput = req.body.txtName
+        const priceInput = req.body.txtPrice
+        const image = req.body.txtImage
+        const Description = req.body.txtDescription
+        const Category = req.body.Category
+        const CategoryID = await dbHandler.getDocumentByName("Category", Category)
+        const UpdateValue = { $set: { name: nameInput, des: Description, price: Number.parseFloat(priceInput), pic: image, category: CategoryID._id } }
+        const id = req.body.txtid
+        console.log(UpdateValue)
+        console.log(id)
+        await dbHandler.updateDocument(id, UpdateValue, "Book")
+        res.redirect('/admin/product')
+    })
+    //update profile for admin
+router.get('/updateprofile', async(req, res) => {
     const result = await dbHandler.getUser(req.session.user.name)
-    res.render('Updateprofileadmin', {user:result})
+    res.render('Updateprofileadmin', { user: result })
 })
-router.post("/updateprofile", async (req,res)=>{
+router.post("/updateprofile", async(req, res) => {
     const phone = req.body.txtPhone
     const fullName = req.body.txtName
     const email = req.body.txtEmail
     const user = await dbHandler.getUser(req.session.user.name)
-    const updateValue = {$set: {userName: user.userName, email: email, Name: fullName, phone: phone, role: user.role, password: user.password}}
+    const updateValue = { $set: { userName: user.userName, email: email, Name: fullName, phone: phone, role: user.role, password: user.password } }
     await dbHandler.updateDocument(user._id, updateValue, "Users")
     res.redirect('/admin')
 })
 
 //update status shopping cart
-router.post("/updatestatus", async (req,res)=>{
+router.post("/updatestatus", async(req, res) => {
     const id = req.body.id
     const status = req.body.status
-    const order = await dbHandler.getDocumentById(id,"Customer Order")
+    const order = await dbHandler.getDocumentById(id, "Customer Order")
     order["Status"] = status
-    const neworder = {$set:{user:order.user, books:order.books, totalPrice:order.totalPrice, time:order.time, Status:order.Status}}
+    const neworder = { $set: { user: order.user, books: order.books, totalPrice: order.totalPrice, time: order.time, Status: order.Status } }
     await dbHandler.updateDocument(id, neworder, "Customer Order")
 
     res.redirect('/admin')
@@ -286,7 +303,7 @@ router.post("/updatestatus", async (req,res)=>{
 // });
 
 
-router.get("/:sortBy", async (req, res, next) => {
+router.get("/:sortBy", async(req, res, next) => {
     let result = await dbHandler.getAll("Customer Order");
     if (req.params.sortBy === "today") {
         let today = new Date().toLocaleDateString("vi");
@@ -303,8 +320,7 @@ router.get("/:sortBy", async (req, res, next) => {
         result = result.filter((item) => item.time > week);
         result.forEach((element) => (element.time = element.time.toLocaleString("vi")));
         res.render("adminPage", { customerOrder: result });
-    }
-    else if (req.params.sortBy === "delete") {
+    } else if (req.params.sortBy === "delete") {
         let { id } = req.query; // same as: let id = req.query.id;
         let result = await dbHandler.deleteOne("Customer Order", { _id: ObjectId(id) });
         if (result) {
@@ -312,31 +328,31 @@ router.get("/:sortBy", async (req, res, next) => {
         } else {
             res.send("Cancel error!");
         }
-    }
-    else {
+    } else {
         next("route");
     }
 });
 
-router.get('/manageCustomer', async (req, res) => {
+router.get('/manageCustomer', async(req, res) => {
     result = await dbHandler.getAll("Users");
+
     const arr = result.filter((element) => {
         return element.role === 'Customer'
     });
     arr.forEach((element, index) => {
-        element.index = index+1;
+        element.index = index + 1;
         delete element.password;
         delete element.role;
     })
-    res.render('adminPage', { Customer: arr})
-    
+    res.render('adminPage', { Customer: arr })
+
 })
 
 router.get('/deleteCustomer/:id', async(req, res) => {
     console.log(req.params.id)
     await dbHandler.deleteDocumentById('Users', req.params.id);
     res.redirect('/admin/manageCustomer')
-    
+
 })
 router.get('/deleteCustomer', async(req, res) => {
     res.send("deleteCustomer")
@@ -345,13 +361,13 @@ router.get('/deleteCustomer', async(req, res) => {
 
 
 //view profile
-exports.getProfile = async (req, res) => {
+exports.getProfile = async(req, res) => {
     let aTrainee = await trainee.findOne({ email: req.session.email })
     res.render('traineeProfileUpdate', { aTrainee: aTrainee, loginName: req.session.email });
 }
 
 //update profile
-exports.updateProfile = async (req, res) => {
+exports.updateProfile = async(req, res) => {
     let id = req.body.id;
     let aTrainee = await trainee.findById(id);
     if (req.file) {
--- databaseHandler.js
+++ databaseHandler.js
@@ -13,197 +13,224 @@ async function getdbo() {
     const client = await MongoClient.connect(URL);
     const dbo = client.db(DATABASE_NAME);
     return dbo;
-  }
-  
-  async function insertObject(collectionName, objectToInsert) {
+}
+
+async function insertObject(collectionName, objectToInsert) {
     const dbo = await getdbo();
     const newObject = await dbo
-      .collection(collectionName)
-      .insertOne(objectToInsert);
+        .collection(collectionName)
+        .insertOne(objectToInsert);
     console.log(
-      "Gia tri id moi duoc insert la: ",
-      newObject.insertedId.toHexString()
+        "Gia tri id moi duoc insert la: ",
+        newObject.insertedId.toHexString()
     );
-  }
-  
-  async function searchObjectbyName(collectionName, name) {
+}
+
+async function searchObjectbyName(collectionName, name) {
     const dbo = await getdbo();
     const result = await dbo
-      .collection(collectionName)
-      .find({ name: { $regex: name, $options: "i" } })
-      .toArray();
+        .collection(collectionName)
+        .find({ name: { $regex: name, $options: "i" } })
+        .toArray();
     return result;
-  }
-  
-  async function searchObjectbyID(collectionName, id) {
+}
+
+async function searchObjectbyID(collectionName, id) {
     const dbo = await getdbo();
     const result = await dbo
-      .collection(collectionName)
-      .find({ id: { $regex: id, $options: "i" } })
-      .toArray();
+        .collection(collectionName)
+        .find({ id: { $regex: id, $options: "i" } })
+        .toArray();
     return result;
-  }
-  
-  async function searchObjectbyPrice(collectionName, price) {
+}
+
+async function searchObjectbyPrice(collectionName, price) {
     const dbo = await getdbo();
     const result = await dbo
-      .collection(collectionName)
-      .find({ price: price })
-      .toArray();
+        .collection(collectionName)
+        .find({ price: price })
+        .toArray();
     return result;
-  }
-  
-  async function searchObjectbyCategory(collectionName, category) {
+}
+
+async function searchObjectbyCategory(collectionName, category) {
     const dbo = await getdbo();
     const result = await dbo
-      .collection(collectionName)
-      .find({ category: ObjectId(category) })
-      .toArray();
+        .collection(collectionName)
+        .find({ category: ObjectId(category) })
+        .toArray();
     return result;
-  }
-  
-  async function getAll(collectionName) {
+}
+
+async function getAll(collectionName) {
     const dbo = await getdbo();
     const result = await dbo
-      .collection(collectionName)
-      .find({})
-      .sort({ time: -1 })
-      .toArray();
+        .collection(collectionName)
+        .find({})
+        .sort({ time: -1 })
+        .toArray();
     return result;
-  }
-  
-  async function deleteDocumentById(collectionName, id) {
+}
+
+async function deleteDocumentById(collectionName, id) {
     const dbo = await getdbo();
     await dbo.collection(collectionName).deleteOne({ _id: ObjectId(id) });
-  }
-  
-  async function deleteDocument(collectionName, objectToDelete) {
+}
+
+async function deleteDocument(collectionName, objectToDelete) {
     const dbo = await getdbo();
     await dbo.collection(collectionName).deleteOne(objectToDelete)
-  }
-  
-  async function getDocumentById(id, collectionName) {
+}
+
+async function getDocumentById(id, collectionName) {
     const dbo = await getdbo();
     const result = await dbo
-      .collection(collectionName)
-      .findOne({ _id: ObjectId(id) });
+        .collection(collectionName)
+        .findOne({ _id: ObjectId(id) });
     return result;
-  }
-  
-  async function updateDocument(id, updateValues, collectionName) {
+}
+
+async function updateDocument(id, updateValues, collectionName) {
     const dbo = await getdbo();
     await dbo
-      .collection(collectionName)
-      .updateOne({ _id: ObjectId(id) }, updateValues);
-  }
-  
-  async function updateCart(userName, updateDict) {
+        .collection(collectionName)
+        .updateOne({ _id: ObjectId(id) }, updateValues);
+}
+
+async function updateCart(userName, updateDict) {
     const dbo = await getdbo();
     await dbo
-      .collection("Order")
-      .replaceOne({ user: userName }, updateDict, { upsert: true });
-  }
-  
-  async function getCart(userName) {
+        .collection("Order")
+        .replaceOne({ user: userName }, updateDict, { upsert: true });
+}
+
+async function getCart(userName) {
     const dbo = await getdbo();
     const result = await dbo.collection("Order").findOne({ user: userName });
     return result;
-  }
-  
-  async function findOne(collectionName, findObject) {
+}
+
+async function findOne(collectionName, findObject) {
     const dbo = await getdbo();
     const result = await dbo.collection(collectionName).findOne(findObject);
     return result;
-  }
-  
-  async function deleteOne(collectionName, deleteObject) {
+}
+
+async function deleteOne(collectionName, deleteObject) {
     const dbo = await getdbo();
     const result = await dbo.collection(collectionName).deleteOne(deleteObject);
     if (result.deletedCount > 0) {
-      return true;
+        return true;
     } else {
-      return false;
+        return false;
     }
-  }
-  
-  async function find(collectionName, findObject) {
-      const dbo = await getdbo();
-      // .sort({_id : -1}) mean is sort newest to oldest
-      // .sort({_id : 1}) is the opposite
-      const result = await dbo.collection(collectionName).find(findObject).sort({ time: -1 }).toArray();
-      return result;
-  }
-  
-  async function checkUserRole(nameIn) {
+}
+
+async function find(collectionName, findObject) {
+    const dbo = await getdbo();
+    // .sort({_id : -1}) mean is sort newest to oldest
+    // .sort({_id : 1}) is the opposite
+    const result = await dbo.collection(collectionName).find(findObject).sort({ time: -1 }).toArray();
+    return result;
+}
+
+async function checkUserRole(nameIn) {
     const dbo = await getdbo();
     const user = await dbo.collection("Users").findOne({ userName: nameIn });
     if (user == null) {
-      return -1;
+        return -1;
     } else {
-      return user.role;
+        return user.role;
     }
-  }
-  
-  async function checkUserLogin(nameIn) {
+}
+
+async function checkUserLogin(nameIn) {
     const dbo = await getdbo();
     const results = await dbo.collection("Users").findOne({ userName: nameIn });
     if (results) {
-      return results;
+        return results;
     } else {
-      return -1;
+        return -1;
     }
-  }
-  
-  async function checkUser(nameIn) {
+}
+
+async function checkUser(nameIn) {
     const dbo = await getdbo();
     const results = await dbo.collection("Users").findOne({ userName: nameIn });
     if (results != null) {
-      return true;
+        return true;
     } else {
-      return false;
+        return false;
     }
-  }
-  
-  async function saveDocument(collectionName, id, newValue) {
+}
+
+async function saveDocument(collectionName, id, newValue) {
     const dbo = await getDbo();
     await dbo.collection(collectionName).save({ _id: ObjectId(id), newValue });
-  }
-  
-  async function getAllFeedback() {
+}
+
+async function getAllFeedback() {
     const result = await getAll("Feedback");
     result.forEach(
-      (e) => (e.timeString = new Date(e.time).toLocaleString("vi-VN"))
+        (e) => (e.timeString = new Date(e.time).toLocaleString("vi-VN"))
     );
     return result;
-  }
-  
-  async function searchOderByUser(collectionName, user) {
+}
+
+async function searchOderByUser(collectionName, user) {
     const dbo = await getdbo();
     const result = await dbo
-      .collection(collectionName)
-      .find({ user: user })
-      .toArray();
+        .collection(collectionName)
+        .find({ user: user })
+        .toArray();
     return result;
-  }
-  async function getDocumentByName(collectionName, name) {
+}
+async function getDocumentByName(collectionName, name) {
     const dbo = await getdbo();
     const result = await dbo.collection(collectionName).findOne({ name: name });
     return result;
-  }
-  
-  async function getUser(name) {
+}
+
+async function getUser(name) {
     const dbo = await getdbo();
     const result = await dbo.collection("Users").findOne({ userName: name });
     return result;
-  }
-  
-  async function searchHotBooks() {
+}
+
+async function searchHotBooks() {
     const dbo = await getdbo();
     const result = await dbo.collection("Book").find({ hot: "true" }).toArray();
     return result;
-  }
-  
-  module.exports = {
+}
+async function countStatistical() {
+    let result = {};
+    const dbo = await getdbo();
+    const orderCounts = await dbo.collection("Order").countDocuments();
+    result.orderCounts = orderCounts;
+
+    const feedbackCounts = await dbo.collection("Feedback").countDocuments();
+    result.feedbackCounts = feedbackCounts;
+
+    const customerCounts = await dbo.collection("Users").countDocuments();
+    result.customerCounts = customerCounts;
+
+    const bookCounts = await dbo.collection("Book").countDocuments();
+    result.bookCounts = bookCounts;
+
+    return result;
+}
+async function findFeedbackById(id) {
+    const dbo = await getdbo();
+    const feedback = await dbo.collection("Feedback").findOne({ _id: ObjectId(id) })
+    return feedback;
+}
+
+async function updateRepply(id, content) {
+    const dbo = await getdbo();
+    const feedback = await dbo.collection("Feedback").updateOne({ "_id": id }, { $set: content }, { "upsert": true })
+}
+
+module.exports = {
     getDocumentByName,
     getUser,
     saveDocument,
@@ -232,4 +259,7 @@ async function getdbo() {
     searchOderByUser,
     searchHotBooks,
     deleteDocument,
-  };
+    updateRepply,
+    countStatistical,
+    findFeedbackById,
+};
